2de762297ada6c6073ebec59d0a76b45af637d22
cobel2 <tbprass@gmail.com>
2017-06-05 12:55:51 +0700

    ini gw udah pos

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+  <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<title>Selamat Datang Administrator</title>
+<body>
+  <div id="container">
+    <div id="header">
+      <center>
+        <div id="font_header_1">Studi Kasus</div>
+        <div id="font_header_2">Studi Kasus</div>
+        <div id="font_header_3">Aplikasi Nilai Mahasiswa</div>
+        <div id="font_header_4">Aplikasi Nilai Mahasiswa</div>
+      </center>
+    </div>
+<div id="menu">
+<ul>
+  <li><a href="index.php?menu=home&action=tampil"class="menu">Home</a></li>
+  <li><a href="index.php?menu=mahasiswa&action=tampil" class="menu">Mahasiswa</a></li>
+  <li><a href="index.php?menu=mata_kuliah&action=tampil" class="Mata Kuliah">Mata Kuliah</a></li>
+  <li><a href="index.php?menu=nilai&action=tampil" class="menu">Nilai</a></li>
+  <li><a href="keluar.php" class="menu">keluar</a></li>
+</ul>
+</div>
+
+<div id="content">
+<div id="kirikolom">
+  <!--proses-->
+</div>
+
+<div id="kanankolom">
+  <!--cari dan tampil-->
+</div>
+</div>
+
+<div id="footer">
+  &copy.Copyright : Aplikasi Teknologi Online 2013
+</div>
+</body>
+</html>
